eb67ec69faa5b80daff95f77b1a4204a2b0182d1
Elena Litvinova <ivanovaev90@gmail.com>
2023-06-24 18:39:13 +0100

    First start project

--- src/views/components/CoverCard/index.tsx
+++ src/views/components/CoverCard/index.tsx
@@ -1,4 +1,20 @@
-const CoverCard = () => {
+import './style.scss';
+
+interface CoverCardProps {
+  image: string;
+  title: string;
+  subTitle: string;
+  text: string;
+  link: string;
+}
+
+const CoverCard = ({
+  image,
+  title,
+  subTitle,
+  text,
+  link
+}: CoverCardProps) => {
   return (
     <div>African forest</div>
   )
